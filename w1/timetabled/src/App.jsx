fc874817a01d59192574fde409148e1ddedb62b5
Rishabh Mediratta <rishabh@Rishabhs-MacBook-Pro-2.local>
2023-02-25 00:07:29 -0600

    w1

---
+++ w1/timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1> Timetabled </h1>
+      <h2> Hello there! Lets look at this pretty calendar </h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
