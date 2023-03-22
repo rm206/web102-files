f523eeee10d2b07a960f0ed7133fd65e790feb9a
Rishabh Mediratta <rishabh@Rishabhs-MacBook-Pro-2.local>
2023-03-21 20:43:30 -0500

    w5

---
+++ w5/crypto-hustle/src/App.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import CoinInfo from './Components/CoinInfo';
+import { Input } from "semantic-ui-react";
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+const App = () => {
+
+  const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(`https://min-api.cryptocompare.com/data/all/coinlist?&api_key=${API_KEY}`);
+      const json = await response.json();
+      setList(json);
+    }
+    fetchAllCoinData().catch(console.error);
+  }, []);
+
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).fileter((item) =>
+        Object.values(item).join("").toLowerCase().includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData);
+    }
+    else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
+
+  return (
+    <>
+      <h1> Crypto List</h1>
+      <input
+        type="text"
+        placeholder="Search.."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+      {searchInput.length > 0
+        ? filteredResults.map((coin) =>
+          list.Data[coin].PlatformType === "blockchain" ?
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
+            : null
+        )
+        : list && Object.entries(list.Data).map(([coin]) =>
+          list.Data[coin].PlatformType === "blockchain" ?
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
+            : null
+        )}
+
+    </>
+  )
+}
+
+export default App;
